0cd46f200beea01d240e95956aaecadabf3a7b40
Finermain <RedHood0878@yandex.ru>
2024-10-27 22:54:09 +0300

    Разработанны задачи 1 и 2. Также расписаны комментарии и заданы описания функций

---
+++ task_01.py
@@ -0,0 +1,37 @@
+import string
+
+def is_palindrome(request):
+    """Функция is_palindrome принимает значение и проверяет палиндроммность."""
+    request = str(request) #Переводит любой тип данных в слово
+
+    request = request.lower() #Делает слово с маленькой буквы
+
+    #Проходим по константе знаков припинания и убираем их
+    for punctuation in string.punctuation:
+        request = request.replace(punctuation , "")
+
+    request = request.replace(" " , "") #Убирает пробелы
+
+    i = 0
+    j = len(request) - 1
+    request_palindrome = True #Переменная в цикле для проверки слова
+    while i < j:
+        if request[i] != request[j]: #Если символ от конца и от начала не совпадают 
+            request_palindrome = False #Он будет отмечать что это не палиндром
+
+        i += 1
+        j -= 1
+
+    #Проверяет результат после цикла и выводит
+    if request_palindrome:
+        print(True)
+        
+    else:
+        print(False)
+
+#Примеры которые должны проверять task_xx.py
+is_palindrome("A man, a plan, a canal -- Panama") # => True
+is_palindrome("Madam, I'm Adam!") # => True
+is_palindrome(333) # => True
+is_palindrome(None) # => False
+is_palindrome("Abracadabra") # => False
---
+++ task_02.py
@@ -0,0 +1,31 @@
+def coincidence(*args):
+    """Функция для определения элементов, значения которого входят в указанный диапазон."""
+    answers = [] #Объявление массива
+    
+    #Всегда кроме передачи двух значений он возвращает пустой массив
+    if len(args) == 1:
+        print(answers)
+        
+    elif len(args) == 2:
+        #Блок где при ошибках возвращает пустой массив
+        try:
+            #Перебираем list
+            for num in args[0]:
+                #Если значение число или число с плавающей точкой
+                if type(num) is int or type(num) is float:
+                    #проверяет входит ли в диапозон 
+                    if args[1][0] <= num <= args[1][-1]:
+                        answers.append(num) #Добавляет значение в список
+            
+            print(answers)
+            
+        except:
+            print(answers)
+            
+    else:
+        print(answers)
+           
+#Примеры которые должны проверять task_xx.py
+coincidence([1, 2, 3, 4, 5], range(3, 6)) # => [3, 4, 5]
+coincidence() # => []
+coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4)) # => [1, 2, 2.5]
